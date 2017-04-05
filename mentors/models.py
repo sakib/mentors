ff081fe6de6acee175ca0033fed86bfa0ce729d5
Sakib Jalal <sakib.jalal@gmail.com>
2017-04-05 19:34:16 -0400

    aaa

--- mentors/models.py
+++ mentors/models.py
@@ -6,3 +6,16 @@ class Mentor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
     email = db.Column(db.String(64), default=None)
+
+
+class MentorSkills(db.Model):
+    __tablename__ = 'mentor_skills'
+    id = db.Column(db.Integer, primary_key=True)
+    mentor_id = db.Column(db.Integer, db.ForeignKey('mentors.id'), nullable=False)
+    skill_id = db.Column(db.Integer, db.ForeignKey('skills.id'), nullable=False)
+
+
+class Skill(db.Model):
+    __tablename__ = 'skills'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
--- mentors/views.py
+++ mentors/views.py
@@ -1,8 +1,8 @@
 #!venv/bin/python
-from flask import redirect, url_for, render_template, request
+from flask import redirect, url_for, render_template, request, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
 from mentors import app, db
-from models import Mentor
+from models import *
 from collections import defaultdict
 import os, json, requests, pprint
 from datetime import timedelta
@@ -13,7 +13,40 @@ pp = pprint.PrettyPrinter(indent=4)
 
 @app.route('/')
 def index():
+    # display mentors and allow searches by skill
     return render_template('index.html')
+
+
+# API endpoint
+@app.route('/mentors', methods=['GET', 'POST'])
+def mentors():
+    if request.method == 'GET':
+        mentors = Mentor.query.all()
+        json_mentors = map(get_mentor_json, mentors)
+        return jsonify(mentors=json_mentors)
+    if request.method == 'POST':
+        name = request.json.get('name')
+        email = request.json.get('email')
+        mentor = Mentor(name=name, email=email)
+        db.session.add(mentor)
+        db.session.commit()
+        return "success!"
+
+
+# Manual add mentors form
+@app.route('/addmentors', methods=['GET','POST'])
+def addmentors():
+    # support adding mentors
+    if request.method == 'GET':
+        return render_template('addmentors.html')
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        mentor = Mentor(name=name, email=email)
+        db.session.add(mentor)
+        db.session.commit()
+        print name, email, " just signed up as a mentor!"
+        return render_template('index.html', name=name)
 
 
 @app.errorhandler(404)
@@ -24,3 +57,9 @@ def page_not_found(e):
 @app.errorhandler(Exception)
 def unhandled_exception(e):
     return render_template('500.html'), 500
+
+
+def get_mentor_json(mentor):
+    return {'id': mentor.id,
+            'name': mentor.name,
+            'email': mentor.email}
